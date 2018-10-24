81c7bf3bfc2d3171184a9169398f93972d16bca8
Øyvind Langeggen <oyvind@imbera.no>
2018-10-24 10:26:43 +0200

    Copy content over to proxy.php from proxy3.php.

--- app/public/proxy3.php
+++ app/public/proxy3.php
@@ -1,19 +1,28 @@
 <?php
 
-$endpoint =  $_GET['method']; 
-
+$endpoint =  $_GET['method'];
+$salonId = $_GET['salonId'];
 $url = "https://booking.raise.no/api/v2/";
-
 $payload = trim(file_get_contents("php://input"));
 
-$response = forward($url, $endpoint, $payload);
-// $resArr = array();
-// $resArr = json_decode($response);
-// echo "<pre>"; print_r($resArr); echo "</pre>";
+$token = findAPIKey($salonId);
+
+$response = forward($url, $endpoint, $payload, $token);
 echo $response;
 
-function forward($url, $endpoint, $payload) {
-    $token = "FC0DC4DA-EA2C-40A5-B6E3-DCC2486094B0";
+function findAPIKey($salonId) {
+    $json = json_decode(file_get_contents('salons.json'));
+    $token = "";
+    
+    foreach($json->Salons as $item) {
+        if($item->Id == $salonId) {
+            return $item->APIKey;
+        }
+    }
+}
+
+function forward($url, $endpoint, $payload, $token) {
+    // $token = "FC0DC4DA-EA2C-40A5-B6E3-DCC2486094B0";
     $authorization = "Authorization: Bearer " . $token;
     $redirect_url = $url . $endpoint;
 
@@ -26,7 +35,6 @@ function forward($url, $endpoint, $payload) {
     ); 
 
     $ch = curl_init($redirect_url);
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));
     curl_setopt_array($ch, $options);
     $response = curl_exec($ch);
 
