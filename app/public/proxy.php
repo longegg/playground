7460b8ab07edd56935a59ec157365927b391adb0
Øyvind Langeggen <oyvind@imbera.no>
2018-10-25 11:42:42 +0200

    Implement session handling in proxy
    The session handling needs further refinement,
    but is working as a test.

--- app/public/proxy.php
+++ app/public/proxy.php
@@ -2,17 +2,35 @@
 
 session_start();
 
+$sessionTimeout = 10;
+
+updateSessionActivity(20);
+resetSessionId($sessionTimeout);
+
 $requestType = $_SERVER['REQUEST_METHOD'];
 $endpoint =  $_GET['method'];
 $salonId = $_GET['salonId'];
 $url = "https://booking.raise.no/api/v2/";
 $payload = trim(file_get_contents("php://input"));
 $isPost = $requestType == "POST";
-// $isGet = $requestType == "GET";
 $querystring = stripQueryParams($_SERVER['QUERY_STRING']);
 $sessionCustomerId = "customerId";
+$lockedEndpoints = array("locked", "customer/2180057/activities");
+
+if (endpointIsLocked($endpoint, $lockedEndpoints)) {
+    if (!isAuthenticated($sessionCustomerId)) {
+        echo "You are not logged in";
+        return;
+    }
+}
+
+if ($endpoint == "customer/isAuthenticated") {
+    echo isAuthenticated($sessionCustomerId)  == null ? "false" : "true";
+    return;
+}
 
 $token = findAPIKey($salonId);
+
 $response = forward($url, $endpoint, $payload, $token, $isPost, $querystring);
 
 if ($endpoint == "customer/search") {
@@ -20,23 +38,45 @@ if ($endpoint == "customer/search") {
     createSession($customerId, $sessionCustomerId);
 }
 
-if ($endpoint == "customer/isAuthenticated") {
-    echo retriveSession($sessionCustomerId);
-    return;
-}
-
 echo $response;
 
-function retriveSession($name) {
-    if (isset($_SESSION[$name])) {
-        return $_SESSION[$name];
+function endpointIsLocked($endpoint, $lockedEndpoints) {
+    return (in_array($endpoint, $lockedEndpoints));
+}
+
+function updateSessionActivity ($timeout) {
+    if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > $timeout)) {
+        // Last request was more than n minutes ago.
+        session_unset();
+        session_destroy();
+    }
+    $_SESSION['LAST_ACTIVITY'] = time();
+}
+
+function resetSessionId($timeout) {
+    if (!isset($_SESSION['CREATED'])) {
+        $_SESSION['CREATED'] = time();
+    } else if (time() - $_SESSION['CREATED'] > $timeout) {
+        // Session started more than n minutes ago.
+        session_regenerate_id(true);
+        $_SESSION['CREATED'] = time();
+    } 
+}
+
+function retriveSession($sessionName) {
+    if (isset($_SESSION[$sessionName])) {
+        return session_id(); //$_SESSION[$sessionName];
     }
     return null;
 }
 
-function createSession($customerId, $name) {
-    if (!isset($_SESSION[$name])) {
-        $_SESSION[$name] = $customerId;
+function isAuthenticated($sessionName) {
+    return retriveSession($sessionName) != null;
+}
+
+function createSession($customerId, $sessionName) {
+    if (!isset($_SESSION[$sessionName])) {
+        $_SESSION[$sessionName] = $customerId;
     } 
 }
 
