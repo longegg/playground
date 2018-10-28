64536153e5f22848853c412ca421b4b16f9132e4
Øyvind Langeggen <oyvind@imbera.no>
2018-10-28 11:55:25 +0100

    Remove SQL folder from git repository

--- app/sql/local.wp_users.sql
+++
@@ -1,5 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_users` VALUES
-(1,"admin","$P$B0.uOIOhK1kVvQO77mmPIdbqM8i0AC0","admin","dev-email@flywheel.local","","2018-10-18 08:35:56","",0,"admin");
